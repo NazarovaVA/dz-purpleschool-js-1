589696673d9bf76a74edc12e2e527c68167956a8
NazarovaVA <nazarovava@internet.ru>
2026-02-04 16:16:05 +0300

    4.Основы

---
+++ 4-navigator/1.js
@@ -0,0 +1,5 @@
+let positionLat = 10;
+let positionLong = 12;
+let addressLat = 13;
+let addressLong = 16;
+console.log(((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2) ** 0.5);
